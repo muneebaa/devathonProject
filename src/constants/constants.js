a9dbd40a7369c3b617cb00cdd3ac962104682a10
mueebaa <muneeba.dilawaze@gmail.com>
2024-09-07 12:10:09 +0500

    constant file for colors added

---
+++ src/constants/constants.js
@@ -0,0 +1,14 @@
+const COLORS = {
+  primary: "#2BAFC7",
+  secondary: "#fff",
+  lightBlue: "#1ACEEF",
+  tranparentBlue: "#2BAFC70F",
+  green: "#2BC7A5CC",
+  darkGreen: "#21C465",
+  red: "#E53535",
+  gray100: "#E4DFDF",
+  gray600: "#747688",
+  gray800: "#5B5B5B",
+};
+
+export default COLORS;
